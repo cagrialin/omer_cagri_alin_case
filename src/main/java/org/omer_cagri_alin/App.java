dff8b5e6256f3010db05f6700e84ae1b17b4e3b4
cagrialin <cagrialinn@gmail.com>
2025-10-26 22:03:00 +0300

    project was created

---
+++ src/main/java/org/omer_cagri_alin/App.java
@@ -0,0 +1,13 @@
+package org.omer_cagri_alin;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
